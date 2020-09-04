1609b7c59e947ee65df50f33e0b4bc96e21155ce
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2020-09-04 13:55:44 -0300

    Edit add and delete images

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -48,10 +48,19 @@ module.exports = {
     product.old_price = formatPrice(product.old_price)
     product.price = formatPrice(product.price)
 
+    // get category
     results = await Category.all()
     const categories = results.rows
 
-    return res.render("products/edit.njk", { product, categories })
+    // get images
+    results = await Product.files(product.id) 
+    let files = results.rows
+    files = files.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`
+    }))
+
+    return res.render("products/edit.njk", { product, categories, files })
 
   },
 
@@ -59,7 +68,7 @@ module.exports = {
     const keys = Object.keys(req.body)
 
     for (key of keys) {
-      if (req.body[key] == "") {
+      if (req.body[key] == "" && key != "removed_files") {
         return res.send('Please, fill all fields')
       }
     }
@@ -68,8 +77,24 @@ module.exports = {
 
     if (req.body.old_price != req.body.price) {
       const oldProduct = await Product.find(req.body.id)
-
       req.body.old_price = oldProduct.rows[0].price
+    }
+
+    if (req.files.length != 0) {
+      const newFilesPromise = req.files.map(file => 
+        File.create({...file, product_id: req.body.id}))
+      
+        await Promise.all(newFilesPromise)
+    }
+
+    if (req.body.removed_files) {
+      const removedFiles = req.body.removed_files.split(",")
+      const lastIndex = removedFiles.length - 1
+      removedFiles.splice(lastIndex, 1)
+
+      const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+      await Promise.all(removedFilesPromise)
     }
 
     await Product.update(req.body)
