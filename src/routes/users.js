abd1bf8ea6455da091a558454fd2e5582f5f5a3f
GabrielZyr <64475947+Gabrielzyr@users.noreply.github.com>
2020-09-06 09:11:17 -0300

    New db for users

---
+++ src/routes/users.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+// login/logout UserController
+
+// routes.get('/login', SessionController.loginForm)
+// routes.post('/login', SessionController.login)
+// routes.post('/logout', SessionController.logout)
+
+// // reset password / forgot
+
+// routes.get('/forgot-password', SessionController.forgotForm)
+// routes.get('/password-reset', SessionController.logout)
+// routes.post('/forgot-password', SessionController.forgot)
+// routes.post('/password-reset', SessionController.reset)
+
+// // user register UserController
+
+// routes.get('/register', UserController.registerForm)
+// routes.post('/register', UserController.post)
+
+// routes.get('/', UserController.show)
+// routes.put('/', UserController.put)
+// routes.delete('/', UserController.delete)
+
+
+
+module.exports = routes
